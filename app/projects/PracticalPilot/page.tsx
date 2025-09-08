3b3feb52a9fef020bb87aa2fa41a6c788630426b
FraserShimmins <shimmins.f@gmail.com>
2025-09-08 22:55:07 +0100

    Fix Linter errors which prevented the PracticalPilot page from being deployed

--- app/projects/PracticalPilot/page.tsx
+++ app/projects/PracticalPilot/page.tsx
@@ -54,7 +54,7 @@ export default function Page()
                 <div className="flex flex-col gap-5 justify-start rounded-xl w-full text-white text-sm md:text-lg lg:text-xl">
                     {/* Intro Paragraph*/}
                     <p>
-                        Practical Pilot - A-Level Physics practicals, streamlined.
+                        <span className="text-[#4ba351] font-bold">Practical Pilot</span> - A-Level Physics practicals, <span className="text-[#4ba351] font-bold">streamlined.</span>
                     </p>
 
                     <p>
@@ -63,7 +63,7 @@ export default function Page()
                     </p>
 
                     <p>
-                        This project was my Coursework for my OCR A-Level in Computer Science. For which both my program and it's corresponding
+                        This project was my Coursework for my OCR A-Level in Computer Science. For which both my program and it&apos;s corresponding
                         write-up <span className="text-[#4ba351] font-bold">achieved an impressive 100% </span>.
                     </p>
 
@@ -72,33 +72,33 @@ export default function Page()
                     </p>
 
                     <p>
-                        A <span className="text-[#4ba351] font-bold">"Welcome Screen"</span> which contained buttons to navigate between core functionality and included a built in Video Tutorial.
-                        Which when clicked created a temporary video player in the application using the 'vlc' library in python. Allowing for easy
+                        A <span className="text-[#4ba351] font-bold">&quot;Welcome Screen&quot;</span> which contained buttons to navigate between core functionality and included a built in Video Tutorial.
+                        Which when clicked created a temporary video player in the application using the &apos;vlc&apos; library in python. Allowing for easy
                         teaching of core program features.
                     </p>
 
                     <p>
-                        A <span className="text-[#4ba351] font-bold">"Exprement Screen"</span> containing a large table of results for users to input experimental data into. This screen contained many unique features,
-                        validation for significant figures of the user's input ensuring quality of scientific data entered as well as significant figure correction for data.
+                        A <span className="text-[#4ba351] font-bold">&quot;Exprement Screen&quot;</span> containing a large table of results for users to input experimental data into. This screen contained many unique features,
+                        validation for significant figures of the user&apos;s input ensuring quality of scientific data entered as well as significant figure correction for data.
                         Automatic calculation for the means and uncertainties of data results was also added, along with data outlier detection, where users can dynamically set
                         the strength of outlier detection and highlight erroneous data in red.
                     </p>
 
                     <p>
-                        A <span className="text-[#4ba351] font-bold">"Graph Screen"</span> which allows users to plot their previously collected data onto scatter graphs. Optimal Lines Of Best Fit (LOBF) can be automatically 
+                        A <span className="text-[#4ba351] font-bold">&quot;Graph Screen&quot;</span> which allows users to plot their previously collected data onto scatter graphs. Optimal Lines Of Best Fit (LOBF) can be automatically 
                         generated for the data, with sliders being added in later iterations to allow users to manually adjust these LOBFs to better suit trends when extreme outliers
-                        are present in the data. Much like the "Experiment Screen" outliers of variying strength can be spotted in these graphs. Error bars can also be plotted to 
+                        are present in the data. Much like the &quot;Experiment Screen&quot; outliers of variying strength can be spotted in these graphs. Error bars can also be plotted to 
                         graphically display uncertainty in the data.
                     </p>
 
                     <p>
-                        A <span className="text-[#4ba351] font-bold">"Report Generation Screen"</span> was created to allow users to easily compile all data colleted and analised on my program into custom formatted pdfs which
+                        A <span className="text-[#4ba351] font-bold">&quot;Report Generation Screen&quot;</span> was created to allow users to easily compile all data colleted and analised on my program into custom formatted pdfs which
                         can be later printed off and worked with offline. Users can toggle which features are included in the resulting pdf report and also select from aprox 12 pre-made
-                        diagrams of "Required Practicals" present in the A-Level AQA syllabus. 
+                        diagrams of &quot;Required Practicals&quot; present in the A-Level AQA syllabus. 
                     </p>
 
                     <p>
-                        Finally a <span className="text-[#4ba351] font-bold">"History Screen"</span> allowing for users to access previously recorded results. This was achieved through using csv files, with each saved set of results having
+                        Finally a <span className="text-[#4ba351] font-bold">&quot;History Screen&quot;</span> allowing for users to access previously recorded results. This was achieved through using csv files, with each saved set of results having
                         a title, topic of study, save date and user written description. The famous algorithms Merge Sort and Binary Search were implemented to allow users to change the order
                         of displayed results and search for specific sets of results.
                     </p>
@@ -110,11 +110,15 @@ export default function Page()
                 </div>
 
                 {/* BUTTON TO SEE GITHUB REPO */}
-                <Link href="/projects/PracticalPilot" className="px-2 py-2 w-fit rounded-xl bg-[#2d755c] text-white">
+                <Link href="https://youtu.be/rvPY4ZxT3XA" className="px-2 py-2 w-fit rounded-xl bg-[#2d755c] text-white">
                     <div className="bg-[#00140d] hover:bg-slate-800 active:bg-slate-900 rounded-xl px-4 py-4 text-xl sm:text-3xl lg:text-4xl">
-                        Check Out The Code!
+                        Check Out A Video Demonstration!
                     </div>
                 </Link>
+
+                <p className="text-red-700 text-sm md:text-lg lg:text-xl font-bold">
+                    *No Github repo is provided for this project due to privacy regulations with Offically Graded A-Level Courseworks
+                </p>
 
 
             </div>
