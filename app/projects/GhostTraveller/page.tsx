6e9addfd20613fb5dfdba6d34b67a676cede1215
FraserShimmins <shimmins.f@gmail.com>
2025-09-09 00:52:57 +0100

    Complete the write up of the Ghost Traveller Project ready for deployment

--- app/projects/GhostTraveller/page.tsx
+++ app/projects/GhostTraveller/page.tsx
@@ -54,28 +54,53 @@ export default function Page()
                 <div className="flex flex-col gap-5 justify-start rounded-xl w-full text-white text-sm md:text-lg lg:text-xl">
                     {/* Intro Paragraph*/}
                     <p>
-                        Ghost Traveller - A 3D Point-And-Click puzzle game.
+                        <span className="text-[#4ba351] font-bold">Ghost Traveller</span> - A 3D <span className="text-[#4ba351] font-bold">Point-And-Click</span> puzzle game.
                     </p>
 
                     <p>
-                        Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo 
-                        Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo 
-                        Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo vv
-                        Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo 
+                        <span className="text-[#4ba351] font-bold">Ghost Traveller</span> is a puzzle game based in purgatory where the player must complete 
+                        <span className="text-[#4ba351] font-bold">5 puzzle sections</span> (each colour coded) inorder to finally unlock the door to the house of the player 
+                        and regain your memories, escaping purgatory. The player is prevented from entering any other section until the current puzzle is solved. Only nearby tiles 
+                        are rendered and revealed to the player, as to not reveal the entirety of the map to the player apon loading in and to create a mysterious atmosphere.
                     </p>
 
                     <p>
-                        Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo 
-                        Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo 
-                        Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo vv
-                        Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo 
+                        Various <span className="text-[#4ba351] font-bold">letters</span> can be found on the ground in the game, which when stepped on will cover the screen with a written note. These are often used to explain
+                        puzzles and give insights into controls.
+                    </p>
+                    <p>
+                        The <span className="text-[#4ba351] font-bold">Green &quot;Forest&quot; section</span>, is the begining section and simply means to teach the player how to control the ghost by making them collect coins and
+                        navigate throughout the map to find various hidden coins. However, since the player agent has pathfinding to where the user clicks, this is often an easy 
+                        task, however this is not intended to be hard, as it acts as the tutorial.
                     </p>
 
                     <p>
-                        Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo 
-                        Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo 
-                        Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo vv
-                        Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo Demo demo 
+                        The <span className="text-[#4ba351] font-bold">brown &quot;Cave&quot; section</span>, is a riddle room. A riddle retelling the story of the players last day alive is shown, with objects and items from this day literring the cave.
+                        Pressing on the buttons next to the objects in the order in which they where encountered in the player&apos;s final day, will unlock the next door.
+                    </p>
+
+                    <p>
+                        The <span className="text-[#4ba351] font-bold">purple &quot;Crystal&quot; section</span>, is a puzzle of &quot;Complete the Circuit&quot;. It contains 3 rooms, each of larger size, containing circuits of differnt wire pieces.
+                        These pieces can be straight lines, tri-connectors, quad-connectors, corner-connectors etc. The player pressing &quot;E&quot; can rotate this circuit tiles to try and
+                        create a direct path from the yellow power source to the target node at the end of the room. A button exists in the room, which will send power from the yellow
+                        source into the circuit to test if a complete path to the end has been created. Once powered fully, the puzzle locks in place and unlocks the door to the next circuit 
+                        / puzzle.
+                    </p>
+
+                    <p>
+                        The <span className="text-[#4ba351] font-bold">red &quot;Lava&quot; section</span>, is a puzzle of a hidden pathway. The ground ahead of the player is fragile and could crumble beneath their ghostly body. Only a predeterminded 
+                        path is safe, with all other tiles sending the player back to the start of the puzzle section, however any path or lava revealed will remain for their next attempt. 
+                        Flowers are shown to grow on solid ground and can be used to try predict the direction of the path and chart out a course to the other side of the puzzle room. Once reaching 
+                        the end, the next puzzle will unlock.
+                    </p>
+
+                    <p>
+                        The <span className="text-[#4ba351] font-bold">black &quot;Lights&quot; section</span>, is a puzzle of &quot;Simon Says&quot;. Where the player will face 4 memory games of simon says of increasing difficulty. Each room having more buttons which
+                        can be used in sequences and longer sequences. Each attempt is entirely randomised as to prevent players from simply brute forcing the puzzle and winning in a non-skillful way.
+                    </p>
+
+                    <p>
+                        This game was created inorder for me to <span className="text-[#4ba351] font-bold">experiment with 3D games</span> in Unity and allowed me to use different aspects of the Unity engine not present in my 2D card game <span className="text-[#4ba351] font-bold">&quot;Heroes & Horrors&quot;</span>.
                     </p>
                 </div>
 
