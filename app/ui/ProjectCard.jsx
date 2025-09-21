a2593fda04c7a79c19935c705a289897b564bb05
FraserShimmins <shimmins.f@gmail.com>
2025-09-21 20:29:51 +0100

    Add .ico file

--- app/ui/ProjectCard.jsx
+++ app/ui/ProjectCard.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import { MagnifyingGlassIcon, FilmIcon } from "@heroicons/react/24/outline";
 import Link from 'next/link';
  
 export default function ProjectCard({ imgUrl, pageUrl, title, description }){
